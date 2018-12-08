2dc3498f79f9416796b740fdbc835bde8a795303
Joakim Ekroos <joakim.ekroos@gmail.com>
2018-12-08 17:05:54 +0200

    Add menu and content container to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+import Menu from './components/Menu';
+import Content from './components/Content';
+
 class App extends Component {
   render (){
     return (
-      <div
-        className="app">
+      <div className="app">
+        <Menu />
+        <Content />
       </div>
     )
   }
