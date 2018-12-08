a6d7705a5271ea11473a5ae891f8faf4ba2bb363
Joakim Ekroos <joakim.ekroos@gmail.com>
2018-12-08 18:26:42 +0200

    Bootstrapify the menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,46 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import { Container, Navbar, NavbarToggler, Collapse, Nav, NavItem } from 'reactstrap';
 
-const Menu = () => {
-  return (
-    <ul>
-      <li>
-        <NavLink
-          to="/"
-          activeClassName="active">
-          Home
-        </NavLink>/
-      </li>
-      <li>
-        <NavLink
-          to="/band"
-          activeClassName="active">
-          The Band
-        </NavLink>/
-      </li>
-      <li>
-        <NavLink
-          to="/people"
-          activeClassName="active">
-          The People
-        </NavLink>/
-      </li>
-      <li>
-        <NavLink
-          to="/music"
-          activeClassName="active">
-          The Music
-        </NavLink>/
-      </li>
-      <li>
-        <NavLink
-          to="/contact"
-          activeClassName="active">
-          Contact
-        </NavLink>/
-      </li>
-    </ul>
-  )
+export default class Menu extends Component{
+
+  constructor(props){
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    }
+  }
+
+  toggle = function(){
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render(){
+    return (
+      <Container>
+        <Navbar light expand="md">
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink
+                to="/"
+                exact
+                activeClassName="active">
+                Home
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                to="/band"
+                activeClassName="active">
+                The Band
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                to="/people"
+                activeClassName="active">
+                The People
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                to="/music"
+                activeClassName="active">
+                The Music
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                to="/contact"
+                activeClassName="active">
+                Contact
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </Container>
+    )
+  }
 }
-
-export default Menu;
