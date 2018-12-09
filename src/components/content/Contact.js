10ef131cb4d507f925d57c213f3d9b5629ac2393
Joakim Ekroos <joakim.ekroos@gmail.com>
2018-12-09 20:03:32 +0200

    Add content to "Contact" page

--- src/components/content/Contact.js
+++ src/components/content/Contact.js
@@ -5,10 +5,34 @@ const Contact = () =>{
   return(
     <div className="thehawkans-content">
       <Container>
-        <p
-          className="slogan">
-          Get in touch
-        </p>
+        <div className="row">
+          <div className="col-md-6">
+            <p>
+              <span className="emphasis">@: </span>henrik.castell@gmail.com
+            </p>
+            <p>
+              <span className="emphasis">#: </span>040 5300 125
+            </p>
+          </div>
+          <div className="col-md-6">
+            <p>
+              <span className="emphasis">www: </span>
+              <a href="//thehawkans.com" target="_blank">thehawkans.com</a>
+            </p>
+            <p>
+              <span className="emphasis">facebook: </span>
+              <a href="//facebook.com/thehawkans" target="_blank">facebook.com/thehawkans</a>
+            </p>
+            <p>
+              <span className="emphasis">soundcloud: </span>
+              <a href="//soundcloud.com/thehawkans" target="_blank">soundcloud.com/thehawkans</a>
+          </p>
+            <p>
+              <span className="emphasis">spotify: </span>
+              <a href="//open.spotify.com/artist/1sz1l9exwoVnqDkrYvyLIu" target="_blank">The Hawkans on spotify</a>
+            </p>
+          </div>
+        </div>
       </Container>
     </div>
   )
